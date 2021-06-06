0c6551b97b01e9a6c0cc17f85f4d5010039cfb2d
IgPo <igpo@ukr.net>
2021-06-06 22:29:00 +0300

    First commit

---
+++ MDK-ARM/Metrology_01/stm32f4xx_hal_tim.d
@@ -0,0 +1,34 @@
+metrology_01\stm32f4xx_hal_tim.o: ../Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c
+metrology_01\stm32f4xx_hal_tim.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h
+metrology_01\stm32f4xx_hal_tim.o: ../Core/Inc/stm32f4xx_hal_conf.h
+metrology_01\stm32f4xx_hal_tim.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h
+metrology_01\stm32f4xx_hal_tim.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h
+metrology_01\stm32f4xx_hal_tim.o: ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h
+metrology_01\stm32f4xx_hal_tim.o: ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xe.h
+metrology_01\stm32f4xx_hal_tim.o: ../Drivers/CMSIS/Include/core_cm4.h
+metrology_01\stm32f4xx_hal_tim.o: C:\Program Files\MDK_ARM\ARM\ARMCC\Bin\..\include\stdint.h
+metrology_01\stm32f4xx_hal_tim.o: ../Drivers/CMSIS/Include/cmsis_version.h
+metrology_01\stm32f4xx_hal_tim.o: ../Drivers/CMSIS/Include/cmsis_compiler.h
+metrology_01\stm32f4xx_hal_tim.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
+metrology_01\stm32f4xx_hal_tim.o: ../Drivers/CMSIS/Include/mpu_armv7.h
+metrology_01\stm32f4xx_hal_tim.o: ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+metrology_01\stm32f4xx_hal_tim.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h
+metrology_01\stm32f4xx_hal_tim.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h
+metrology_01\stm32f4xx_hal_tim.o: C:\Program Files\MDK_ARM\ARM\ARMCC\Bin\..\include\stddef.h
+metrology_01\stm32f4xx_hal_tim.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h
+metrology_01\stm32f4xx_hal_tim.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h
+metrology_01\stm32f4xx_hal_tim.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h
+metrology_01\stm32f4xx_hal_tim.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h
+metrology_01\stm32f4xx_hal_tim.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h
+metrology_01\stm32f4xx_hal_tim.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h
+metrology_01\stm32f4xx_hal_tim.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h
+metrology_01\stm32f4xx_hal_tim.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h
+metrology_01\stm32f4xx_hal_tim.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h
+metrology_01\stm32f4xx_hal_tim.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h
+metrology_01\stm32f4xx_hal_tim.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h
+metrology_01\stm32f4xx_hal_tim.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h
+metrology_01\stm32f4xx_hal_tim.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h
+metrology_01\stm32f4xx_hal_tim.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h
+metrology_01\stm32f4xx_hal_tim.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pcd.h
+metrology_01\stm32f4xx_hal_tim.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_usb.h
+metrology_01\stm32f4xx_hal_tim.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pcd_ex.h
---
+++ MDK-ARM/Metrology_01/usbd_custom_hid_if.d
@@ -0,0 +1,46 @@
+metrology_01\usbd_custom_hid_if.o: ../USB_DEVICE/App/usbd_custom_hid_if.c
+metrology_01\usbd_custom_hid_if.o: ../USB_DEVICE/App/usbd_custom_hid_if.h
+metrology_01\usbd_custom_hid_if.o: ../Middlewares/ST/STM32_USB_Device_Library/Class/CustomHID/Inc/usbd_customhid.h
+metrology_01\usbd_custom_hid_if.o: ../Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ioreq.h
+metrology_01\usbd_custom_hid_if.o: ../Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_def.h
+metrology_01\usbd_custom_hid_if.o: ../USB_DEVICE/Target/usbd_conf.h
+metrology_01\usbd_custom_hid_if.o: C:\Program Files\MDK_ARM\ARM\ARMCC\Bin\..\include\stdio.h
+metrology_01\usbd_custom_hid_if.o: C:\Program Files\MDK_ARM\ARM\ARMCC\Bin\..\include\stdlib.h
+metrology_01\usbd_custom_hid_if.o: C:\Program Files\MDK_ARM\ARM\ARMCC\Bin\..\include\string.h
+metrology_01\usbd_custom_hid_if.o: ../Core/Inc/main.h
+metrology_01\usbd_custom_hid_if.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h
+metrology_01\usbd_custom_hid_if.o: ../Core/Inc/stm32f4xx_hal_conf.h
+metrology_01\usbd_custom_hid_if.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h
+metrology_01\usbd_custom_hid_if.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h
+metrology_01\usbd_custom_hid_if.o: ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h
+metrology_01\usbd_custom_hid_if.o: ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xe.h
+metrology_01\usbd_custom_hid_if.o: ../Drivers/CMSIS/Include/core_cm4.h
+metrology_01\usbd_custom_hid_if.o: C:\Program Files\MDK_ARM\ARM\ARMCC\Bin\..\include\stdint.h
+metrology_01\usbd_custom_hid_if.o: ../Drivers/CMSIS/Include/cmsis_version.h
+metrology_01\usbd_custom_hid_if.o: ../Drivers/CMSIS/Include/cmsis_compiler.h
+metrology_01\usbd_custom_hid_if.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
+metrology_01\usbd_custom_hid_if.o: ../Drivers/CMSIS/Include/mpu_armv7.h
+metrology_01\usbd_custom_hid_if.o: ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+metrology_01\usbd_custom_hid_if.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h
+metrology_01\usbd_custom_hid_if.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h
+metrology_01\usbd_custom_hid_if.o: C:\Program Files\MDK_ARM\ARM\ARMCC\Bin\..\include\stddef.h
+metrology_01\usbd_custom_hid_if.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h
+metrology_01\usbd_custom_hid_if.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h
+metrology_01\usbd_custom_hid_if.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h
+metrology_01\usbd_custom_hid_if.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h
+metrology_01\usbd_custom_hid_if.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h
+metrology_01\usbd_custom_hid_if.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h
+metrology_01\usbd_custom_hid_if.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h
+metrology_01\usbd_custom_hid_if.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h
+metrology_01\usbd_custom_hid_if.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h
+metrology_01\usbd_custom_hid_if.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h
+metrology_01\usbd_custom_hid_if.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h
+metrology_01\usbd_custom_hid_if.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h
+metrology_01\usbd_custom_hid_if.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h
+metrology_01\usbd_custom_hid_if.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h
+metrology_01\usbd_custom_hid_if.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pcd.h
+metrology_01\usbd_custom_hid_if.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_usb.h
+metrology_01\usbd_custom_hid_if.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pcd_ex.h
+metrology_01\usbd_custom_hid_if.o: ../Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_core.h
+metrology_01\usbd_custom_hid_if.o: ../Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ioreq.h
+metrology_01\usbd_custom_hid_if.o: ../Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ctlreq.h
